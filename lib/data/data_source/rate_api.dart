7c43604ef10c7e65abc02624476b81a954676f1c
OSS0302 <tkdtn3264@gmail.com>
2024-01-09 16:33:48 +0900

    rate_api 함수 이름 변경

--- lib/data/data_source/rate_api.dart
+++ lib/data/data_source/rate_api.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class RateApi {
   // 여러개 리스트 받을 때 패턴
-  Future<List<Rate>> getTodos2() async {
+  Future<List<Rate>> getRateApi() async {
     final response =
     await http.get(Uri.parse('https://open.er-api.com/v6/latest/KRW'));
     final jsonList = jsonDecode(response.body) as List<dynamic>;
